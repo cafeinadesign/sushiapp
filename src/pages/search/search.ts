69e333f74d8873777201abb87e7fd196756d4cf6
Thiago Prazeres <thiago.prazeres@ufpe.br>
2018-04-23 15:45:37 -0300

    restaurantes

--- src/pages/search/search.ts
+++ src/pages/search/search.ts
@@ -13,17 +13,19 @@ export class SearchPage {
 
   currentItems: any = [];
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public items: Items) { }
+  constructor(public navCtrl: NavController, public navParams: NavParams, public items: Items) {
+    this.currentItems = this.items.query();
+  }
 
   /**
    * Perform a service for the proper items.
    */
   getItems(ev) {
     let val = ev.target.value;
-    if (!val || !val.trim()) {
-      this.currentItems = [];
-      return;
-    }
+    // if (!val || !val.trim()) {
+    //   this.currentItems = [];
+    //   return;
+    // }
     this.currentItems = this.items.query({
       name: val
     });
